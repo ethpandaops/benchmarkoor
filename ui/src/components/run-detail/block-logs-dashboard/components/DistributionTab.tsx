593077d9eb0c0a7f0da37e583231b486c6ea5fdb
Rafael Matias <rafael@skyle.net>
2026-02-03 15:42:11 +0100

    chore(ui): show distribution charts in single column layout

--- ui/src/components/run-detail/block-logs-dashboard/components/DistributionTab.tsx
+++ ui/src/components/run-detail/block-logs-dashboard/components/DistributionTab.tsx
@@ -154,8 +154,8 @@ export function DistributionTab({ data, stats, isDark, useLogScale }: Distributi
         </div>
       )}
 
-      {/* Charts Grid */}
-      <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
+      {/* Charts */}
+      <div className="flex flex-col gap-6">
         <div className="rounded-sm border border-gray-200 bg-white p-4 dark:border-gray-700 dark:bg-gray-800">
           <BoxPlotChart data={data} isDark={isDark} useLogScale={useLogScale} />
         </div>
