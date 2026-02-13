e620196eb6c1177d1335c19fb586e4c99b4f4e49
Rafael Matias <rafael@skyle.net>
2026-02-13 23:35:56 +0100

    fix(executor): apply De Morgan's law to satisfy staticcheck QF1001

--- pkg/executor/source.go
+++ pkg/executor/source.go
@@ -338,7 +338,7 @@ func looksLikeCommitHash(s string) bool {
 	}
 
 	for _, c := range s {
-		if !((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')) {
+		if (c < '0' || c > '9') && (c < 'a' || c > 'f') && (c < 'A' || c > 'F') {
 			return false
 		}
 	}
